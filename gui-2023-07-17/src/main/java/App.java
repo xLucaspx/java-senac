f7a4832c9abfc450c04ec8031ffd8f449a3f67a7
Lucas da Paz <xlucaspx@gmail.com>
2023-07-24 21:23:23 -0300

    quick add projects bc of formatting senac pc

---
+++ gui-2023-07-17/src/main/java/App.java
@@ -0,0 +1,19 @@
+
+import views.Form;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 182220015
+ */
+public class App {
+
+  public static void main(String[] args) {
+    Form f = new Form();
+    f.setVisible(true);
+  }
+}
---
+++ gui-2023-07-18/src/App.java
@@ -0,0 +1,20 @@
+
+import views.FormTable;
+import views.FormTextArea;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 182220015
+ */
+public class App {
+  public static void main(String[] args) {
+    FormTable view = new FormTable();
+    view.setVisible(true);
+  }
+}
